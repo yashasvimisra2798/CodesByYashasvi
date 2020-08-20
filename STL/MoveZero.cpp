05f6508d46c2dcaff16249b2924d6cd6f7e450f0
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-08-20 14:35:19 +0530

    Create MoveZero.cpp

---
+++ STL/MoveZero.cpp
@@ -0,0 +1,25 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Example:
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+// CODE:-
+
+class Solve{
+    public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int n=nums.size(),m=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]!=0)
+              nums[m++]=nums[i];
+        }
+        for(int i=m;i<n;i++)
+        {
+            nums[i]=0;
+        }
+    }
+}
