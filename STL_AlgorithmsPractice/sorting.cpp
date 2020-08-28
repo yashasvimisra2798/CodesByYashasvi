865ea8eb9980ffc71d9e8e9d6dd675aeb13e1467
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-08-28 14:09:53 +0530

    sorting

---
+++ STL_AlgorithmsPractice/sorting.cpp
@@ -0,0 +1,30 @@
+// C++ Program to demostrate sorting using STL algorithms
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[] = {2,3,8,9,6,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+
+    for(i=0;i<n;i++)
+    {
+        cout<<v[i];
+    }
+
+    // Sorting the vector
+    sort(v.begin(),v.end());
+
+    // Printing the vector after sorting
+
+    for(i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+    }
+}
